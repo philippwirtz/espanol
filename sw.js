7ea5ff0767c27d5c93b3a7b3316f9574bc4e1729
philippwirtz <42355477+philippwirtz@users.noreply.github.com>
2026-08-12 14:11:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'espanol-v1';
+const CACHE = 'espanol-v2';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
